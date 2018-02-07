afa0e38dbe779f4eca927044c5f3ece323f7de0b
Chris <31326880+imasmakurmomsdum@users.noreply.github.com>
2018-02-07 14:08:00 -0700

    Fixed error where snake should equal s, but was written out

--- sketch.js
+++ sketch.js
@@ -27,7 +27,7 @@ function pickLocation() {
   var cols = floor(width/scl);
   var rows = floor(height/scl);
   food = createVector(floor(random(cols)), floor(random(rows)));
-  if (food.x === snake.x || food.y === snake.y) {
+  if (food.x === snake.x || food.y === s.y) {
     food = createVector(floor(random(cols)), floor(random(rows)));
   }
   food.mult(scl);
